69cd0b251b45584c84d00c34622adc54f6cfd528
David Lee <aewofij@gmail.com>
2016-01-02 13:15:12 -0500

    remove edges view swipe dismiss behavior

--- pfreddit2/GraphEdgesViewController.swift
+++ pfreddit2/GraphEdgesViewController.swift
@@ -38,39 +38,11 @@ class GraphEdgesViewController: UIViewController {
 	private var _registeredCells: [String: AnyClass?] = [:]
 	private var _registeredNibs: [String: UINib?] = [:]
 
-	override func viewDidLoad() {
-		super.viewDidLoad()
-//		edgesForExtendedLayout = UIRectEdge.None
-		registerDismissRecognizer()
-	}
-
-	func registerDismissRecognizer() {
-		collectionView?.panGestureRecognizer.addTarget(self, action: "handlePanDismiss:")
-	}
-
-	func unregisterDismissRecognizer() {
-		collectionView?.panGestureRecognizer.removeTarget(self, action: "handlePanDismiss:")
-	}
-
 	func registerClass(cell: AnyClass?, forCellWithReuseIdentifier reuseIdentifier: String) {
 		_registeredCells[reuseIdentifier] = cell
 	}
 
 	func registerNib(nib: UINib?, forCellWithReuseIdentifier reuseIdentifier: String) {
 		_registeredNibs[reuseIdentifier] = nib
-	}
-
-	internal func handlePanDismiss(recognizer: UIPanGestureRecognizer) {
-		guard let collectionView = collectionView else { return }
-		switch recognizer.state {
-		case .Ended:
-			let threshold: CGFloat = 100
-			if collectionView.contentOffset.y < 0 && abs(collectionView.contentOffset.y) > threshold {
-				delegate?.dismissGraphEdgesViewController(self, animated: true)
-			}
-
-		default:
-			break
-		}
 	}
 }
